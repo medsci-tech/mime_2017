dc8984ce0681702882acec2d82151cc2d38b4325
PythonCakeMing <1851464029@qq.com>
2017-05-15 14:11:16 +0800

    user roles

---
+++ database/migrations/2017_05_15_055717_create_customer_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('customer_roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('role_en', 30)->unique()->comment('用户角色');
+            $table->string('role_ch', 30)->unique()->comment('用户角色');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('customer_roles');
+    }
+}
