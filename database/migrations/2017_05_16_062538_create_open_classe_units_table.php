46246c6ad274bf1a2b6398fde2b561447f9a6543
PythonCakeMing <1851464029@qq.com>
2017-05-16 14:47:05 +0800

    open class

--- database/migrations/2017_05_16_062538_create_open_classe_units_table.php
+++ database/migrations/2017_05_16_062538_create_open_classe_units_table.php
@@ -14,6 +14,19 @@ class CreateOpenClasseUnitsTable extends Migration
     public function up()
     {
         //
+        Schema::create('open_class_units', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('level')->comment('等级');
+            $table->unsignedInteger('class_count')->default(0)->comment('课程数');
+            $table->unsignedInteger('student_count')->default(0)->comment('学员数');
+            $table->unsignedInteger('score')->default(0)->comment('评分');
+
+            $table->string('abstract_content')->comment('简介');
+            $table->string('abstract_video_url')->comment('宣传视频');
+
+            $table->timestamps();
+        });
     }
 
     /**
@@ -24,5 +37,6 @@ class CreateOpenClasseUnitsTable extends Migration
     public function down()
     {
         //
+        Schema::dropIfExists('open_class_units');
     }
 }
