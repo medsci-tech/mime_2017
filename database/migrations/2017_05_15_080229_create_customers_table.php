569cc9883bd4532b36912fa855d94e52c4ca2b7d
PythonCakeMing <1851464029@qq.com>
2017-05-16 17:03:33 +0800

    customers

--- database/migrations/2017_05_15_080229_create_customers_table.php
+++ database/migrations/2017_05_15_080229_create_customers_table.php
@@ -26,16 +26,17 @@ class CreateCustomersTable extends Migration
             $table->string('name')->comment('名字');
             $table->string('sex')->comment('性别');
             $table->string('age')->comment('年龄');
-            $table->string('title')->comment('职称');
+            $table->unsignedInteger('title_id')->comment('title id');
             $table->string('office')->comment('科室');
             $table->boolean('authorized')->default(False)->comment('是否认证');
 
-            $table->string('inviter_phone', 11)->nullable()->comment('邀请人电话');
+            $table->string('invite_phone', 11)->nullable()->comment('邀请人电话');
 
             $table->timestamps();
 
             $table->foreign('customer_role_id')->references('id')->on('customer_roles');
             $table->foreign('hospital_id')->references('id')->on('hospitals');
+            $table->foreign('title_id')->references('id')->on('customer_titles');
         });
     }
 
@@ -49,6 +50,7 @@ class CreateCustomersTable extends Migration
         //
         Schema::table('customers', function(Blueprint $table) {
             $table->dropForeign('customers_role_id_foreign');
+            $table->dropForeign('customers_title_id_foreign');
             $table->dropForeign('customers_hospital_id_foreign');
         });
         Schema::dropIfExists('customers');
