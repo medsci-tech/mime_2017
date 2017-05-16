a6aa7c4c242fb006073e9de5c16b27ebac8a19be
PythonCakeMing <1851464029@qq.com>
2017-05-16 13:08:46 +0800

    authorized customer projects

--- database/migrations/2017_05_16_032750_create_authorized_customer_projects_table.php
+++ database/migrations/2017_05_16_032750_create_authorized_customer_projects_table.php
@@ -14,6 +14,18 @@ class CreateAuthorizedCustomerProjectsTable extends Migration
     public function up()
     {
         //
+        Schema::create('authorized_customer_projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('customer_id')->comment('用户id');
+
+            $table->string('pub_date')->comment('发布日期');
+            $table->string('pub_journal')->comment('发布期刊');
+            $table->string('pub_title')->comment('文章标题');
+            $table->string('pub_url')->nullable()->comment('文章链接');
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
     }
 
     /**
@@ -24,5 +36,9 @@ class CreateAuthorizedCustomerProjectsTable extends Migration
     public function down()
     {
         //
+        Schema::table('authorized_customer_projects', function(Blueprint $table) {
+            $table->dropForeign('authorized_customer_projects_customer_id_foreign');
+        });
+        Schema::dropIfExists('authorized_customer_projects');
     }
 }
