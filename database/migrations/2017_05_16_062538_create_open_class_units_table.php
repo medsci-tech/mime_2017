e06d5f756e1cc3db5e05d26733f3e98ded0f3bc1
PythonCakeMing <1851464029@qq.com>
2017-05-17 11:37:30 +0800

    open class

--- database/migrations/2017_05_16_062538_create_open_class_units_table.php
+++ database/migrations/2017_05_16_062538_create_open_class_units_table.php
@@ -17,6 +17,7 @@ class CreateOpenClasseUnitsTable extends Migration
         Schema::create('open_class_units', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('disease_id')->comment('疾病类型');
+            $table->unsignedInteger('type_id')->comment('课程分类');
 
             $table->string('title')->unique()->comment('名字');
             $table->string('level')->comment('等级');
@@ -30,6 +31,7 @@ class CreateOpenClasseUnitsTable extends Migration
             $table->string('abstract_video_url')->comment('宣传视频');
 
             $table->foreign('disease_id')->references('id')->on('diseases');
+            $table->foreign('type_id')->references('id')->on('open_class_types');
             $table->timestamps();
         });
     }
@@ -44,6 +46,7 @@ class CreateOpenClasseUnitsTable extends Migration
         //
         Schema::table('open_class_units', function(Blueprint $table) {
             $table->dropForeign('open_class_units_disease_id_foreign');
+            $table->dropForeign('open_class_units_type_id_foreign');
         });
         Schema::dropIfExists('open_class_units');
     }
